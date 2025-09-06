efabcccbe6c2be25f52c744956d8258de4c36122
Cuonq_291 <nguyenncuongg291@gmail.com>
2025-09-07 00:04:55 +0700

    fix: update allow method cors

--- src/main/java/com/example/haus/config/SecurityConfig.java
+++ src/main/java/com/example/haus/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // domain FE
-        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","PATCH","OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
 
