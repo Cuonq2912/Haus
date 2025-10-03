1166bc679b6e347ad214bc83962b9df62763961a
Cuonq_291 <nguyenncuongg291@gmail.com>
2025-10-03 15:01:14 +0700

    feat: update attribute for vnpay config

--- src/main/java/com/example/haus/domain/entity/product/ProductVariation.java
+++ src/main/java/com/example/haus/domain/entity/product/ProductVariation.java
@@ -7,7 +7,7 @@ import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Entity
-@Table(name = "product-variations")
+@Table(name = "product_variations")
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
